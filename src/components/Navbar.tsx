7ff3c48aabe185a532413a941a21b1ad2021b6a5
y8ahmed <y8ahmed@my.yorku.ca>
2023-10-02 23:46:22 -0400

    add thumbnail and favicon, add metadata, add absoluteUrl to trpcbatchClient, mobileNavbar and fix metadata on layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,7 @@ import { buttonVariants } from "./ui/button";
 import { LoginLink, RegisterLink, getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { ArrowRight } from "lucide-react";
 import UserAccountNav from "./UserAccountNav";
+import MobileNav from "./MobileNav";
 
 const Navbar = () => {
   const { getUser } = getKindeServerSession()
@@ -17,7 +18,8 @@ const Navbar = () => {
             <span>Quantum.</span>
           </Link>
 
-          {/* add mobile nav bar */}
+          <MobileNav isAuth={!!user} />
+
           <div className="hidden items-center space-x-4 sm:flex">
           {!user ? (
               <>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
- 
+import { Metadata } from 'next'
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
@@ -10,3 +11,47 @@ export function absoluteUrl(path: string) {
   if (process.env.RENDER_EXTERNAL_URL) return `${process.env.RENDER_EXTERNAL_URL}${path}`
   return `http://localhost:${process.env.PORT ?? 3000}${path}`
 }
+
+export function constructMetadata({
+  title = "Quantum - the SaaS for analysts",
+  description = "Quantum is Chatgpt but for SEC filing and it supports SEDAR filings aswell.",
+  image = "/thumbnail.png",
+  icons = "/favicon.ico",
+  noIndex = false
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+  noIndex?: boolean
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image
+        }
+      ]
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@yusuf88ahmed"
+    },
+    icons,
+    metadataBase: new URL('https://quill-jet.vercel.app'),
+    themeColor: '#FFF',
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false
+      }
+    })
+  }
+};
