3d07bed27dd45298b236f8a5841589d4ae8de6e2
y8ahmed <y8ahmed@my.yorku.ca>
2023-10-03 15:45:30 -0400

    fix absoluteUrl function to use vercel

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,8 +8,11 @@ export function cn(...inputs: ClassValue[]) {
 
 export function absoluteUrl(path: string) {
   if (typeof window !== 'undefined') return path
-  if (process.env.RENDER_EXTERNAL_URL) return `${process.env.RENDER_EXTERNAL_URL}${path}`
-  return `http://localhost:${process.env.PORT ?? 3000}${path}`
+  if (process.env.VERCEL_URL)
+    return `https://${process.env.VERCEL_URL}${path}`
+  return `http://localhost:${
+    process.env.PORT ?? 3000
+  }${path}`
 }
 
 export function constructMetadata({
