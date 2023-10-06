d98b9045143c38dc0f1116b965750d9b2cf5a263
y8ahmed <y8ahmed@my.yorku.ca>
2023-10-05 21:01:22 -0400

    change to pricing width to 7xl

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -116,7 +116,7 @@ const Page = () => {
 
   return (
     <>
-      <MaxWidthWrapper className="mb-8 mt-24 text-center max-w-6xl">
+      <MaxWidthWrapper className="mb-8 mt-24 text-center max-w-7xl">
         <div className="mx-auto mb-10 sm:max-w-lg">
           <h1 className="text-6xl font-bold sm:text-7xl">Pricing</h1>
           <p className="mt-5 text-gray-600 sm:text-lg">
